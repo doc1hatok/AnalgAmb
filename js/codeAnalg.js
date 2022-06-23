7545317acdbd6500ad6c93c8f9a60955bb2fd31c
doc1hatok <76207116+doc1hatok@users.noreply.github.com>
2022-06-23 05:06:41 +0200

    Initial commit

---
+++ js/codeAnalg.js
@@ -0,0 +1,11 @@
+$(document).ready(function() 
+{
+  document.getElementById("fname").focus();
+  
+});
+
+function myFunction() {
+  document.getElementById("hierHin").innerHTML="hallo";
+  document.getElementById("fname").innerHTML="Test";
+ /* document.getElementById("fname").focus();*/
+}
---
+++ js/dbAnalg.js
@@ -0,0 +1,21 @@
+var medBase = [
+
+{Generic: "Ibuprofen",
+Praeparat: "Ibu",
+Menge: "600"
+Einheit: "mg"
+Applikation: "po",
+Applikationsform: "Tbl",
+Dosis: "10",
+Intervall: "8"},
+
+{Generic: "Ibuprofen",
+Praeparat: "Ibu",
+Menge: "400"
+Einheit: "mg"
+Applikation: "po",
+Applikationsform: "Tbl",
+Dosis: "10",
+Intervall: "8"},
+
+]
