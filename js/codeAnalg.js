1921210c53a148e99743a72031e4373805b1bd92
Hakki Toker <htoker@gmx.net>
2022-06-24 08:19:45 +0300

    myFunction2

--- js/codeAnalg.js
+++ js/codeAnalg.js
@@ -1,11 +1,17 @@
-$(document).ready(function() 
+/*$(document).ready(function() 
 {
   document.getElementById("fname").focus();
   
 });
-
+*/
 function myFunction() {
   document.getElementById("hierHin").innerHTML="hallo";
   document.getElementById("fname").innerHTML="Test";
  /* document.getElementById("fname").focus();*/
+}
+
+
+function myFunction2() {
+window.open('index-2.html','_blank', 'location=yes,height=70,width=120,scrollbars=yes,status=yes');	
+
 }
