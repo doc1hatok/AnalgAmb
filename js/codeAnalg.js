437bb6dbfaf3f3468a1e2d1f1c5fb25a2f0ddb18
Hakki Toker <htoker@gmx.net>
2022-06-26 21:59:17 +0300

    Altersberechnung

--- js/codeAnalg.js
+++ js/codeAnalg.js
@@ -1,7 +1,7 @@
 /*$(document).ready(function() 
 {
-  document.getElementById("fname").focus();
-  
+	document.getElementById("startbutton").addEventListener("click", myFunction2);
+/*  
 });
 */
 function myFunction() {
@@ -12,6 +12,38 @@ function myFunction() {
 
 
 function myFunction2() {
-window.open('index-2.html','_blank', 'location=yes,top=200,left=200,height=470,width=520,scrollbars=yes,status=yes');	
+window.open('analg_form.html','_blank', 'location=yes,top=200,left=200,height=470,width=520,scrollbars=yes,status=yes');	
 
+}
+
+function alterString() {
+	 // datepart: 'y', 'm', 'w', 'd', 'h', 'n', 's'
+	Date.dateDiff = function(datepart, fromdate, todate) {	
+	  datepart = datepart.toLowerCase();	
+	  var diff = todate - fromdate;	
+	  var divideBy = { y:86400000*365,
+		  			   m:86400000*30,
+		  			   w:604800000, 
+	                   d:86400000, 
+	                   h:3600000, 
+	                   n:60000, 
+	                   s:1000 };	
+	  
+	  return Math.floor(diff/divideBy[datepart]);
+	}
+	var today= new Date();		
+	var xyz= new Date(document.getElementById("birthday").value);	
+	//console.log(Date.dateDiff("d",xyz,today))
+	return Date.dateDiff('y',xyz,today);
+}
+
+
+function ausgabeBox() {
+	box= "Ambulante Schmerztherapieanordnung für " +
+	document.getElementById("fname").value + " " +
+	document.getElementById("lname").value + "<br>" +
+	//document.getElementById("birthday").value;
+	"Alter: " + alterString();
+  document.getElementById("hierHin").innerHTML=box;	
+  
 }
